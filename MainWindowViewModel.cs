1c791dc723bb70352ea402274e475156189b2d05
raees21 <raeesaziz786@gmail.com>
2022-04-23 13:39:23 +0200

    Initialize Variables

--- MainWindowViewModel.cs
+++ MainWindowViewModel.cs
@@ -9,65 +9,10 @@ namespace Simple_Quotation_Tool
 {
     public class MainWindowViewModel : INotifyPropertyChanged
     {
-        private User user;
-
-        public string FirstName
-        {
-            get { return user.FirstName; }
-            set
-            {
-                if (user.FirstName != value)
-                {
-                    user.FirstName = value;
-                    OnPropertyChange("FirstName");
-                    // If the first name has changed, the FullName property needs to be udpated as well.
-                    OnPropertyChange("FullName");
-                }
-            }
-        }
-
-        public string LastName
-        {
-            get { return user.LastName; }
-            set
-            {
-                if (user.LastName != value)
-                {
-                    user.LastName = value;
-                    OnPropertyChange("LastName");
-                    // If the first name has changed, the FullName property needs to be udpated as well.
-                    OnPropertyChange("FullName");
-                }
-            }
-        }
-
-        // This property is an example of how model properties can be presented differently to the View.
-        // In this case, we transform the birth date to the user's age, which is read only.
-        public int Age
-        {
-            get
-            {
-                DateTime today = DateTime.Today;
-                int age = today.Year - user.BirthDate.Year;
-                if (user.BirthDate > today.AddYears(-age)) age--;
-                return age;
-            }
-        }
-
-        // This property is just for display purposes and is a composition of existing data.
-        public string FullName
-        {
-            get { return FirstName + " " + LastName; }
-        }
+        #region Initialization
 
         public MainWindowViewModel()
         {
-            user = new User
-            {
-                FirstName = "John",
-                LastName = "Doe",
-                BirthDate = DateTime.Now.AddYears(-30)
-            };
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -79,6 +24,64 @@ namespace Simple_Quotation_Tool
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
             }
 
+        }
+
+        #endregion
+
+        private string customerName;
+        public string CustomerName
+        {
+            get { return customerName; }
+            set
+            {
+                if (value != customerName)
+                {
+                    customerName = value;
+                    OnPropertyChange(nameof(CustomerName));
+                }
+            }
+        }
+
+        private string customerCell;
+        public string CustomerCell
+        {
+            get { return customerCell; }
+            set
+            {
+                if (value != customerCell)
+                {
+                    customerCell = value;
+                    OnPropertyChange(nameof(CustomerCell));
+                }
+            }
+        }
+
+        private string customerEmail;
+        public string CustomerEmail
+        {
+            get { return customerEmail; }
+            set
+            {
+                if (value != customerEmail)
+                {
+                    customerEmail = value;
+                    OnPropertyChange(nameof(CustomerEmail));
+                }
+            }
+        }
+
+        private string customerEmail;
+        public string CustomerEmail
+        {
+            get { return customerEmail; }
+            set
+            {
+                if (value != customerEmail)
+                {
+                    customerEmail = value;
+                    OnPropertyChange(nameof(CustomerEmail));
+                }
+            }
         }
     }
 }
